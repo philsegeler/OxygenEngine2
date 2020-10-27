2bb3617d3a3660512b699cae813c4f889af10d13
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2020-10-28 00:47:52 +0100

    Added optimization idea

--- parser_v2/Lexer.h
+++ parser_v2/Lexer.h
@@ -13,14 +13,21 @@
  * new one is effectively the new token (Think about 1. here as well - accessing the heap for every
  * token may be very time consuming).
  *
- * 3. Line number on error: Don't continuously increment the line counter. Instead, look for the line
- * and column the character Pointer points to, once an error occurs.
+ * 3. Line number on error: Don't continuously increment the line counter. Instead, look for the
+ * line and column the character Pointer points to, once an error occurs.
  * 
  * 4. Look for an efficient binary compare function. With this, double-character-operators could
  * be checked for, without the need for backtracking. Also: find patterns in the ascii values
  * of the strings to be compared, to make things more efficient (In order to check whether a
  * character is a number, it doesn't have to be checked against every number, only against the
  * first and the last one on the ascii table).
+ *
+ * 5. User smart pointers in order to minimize memory leaks (Conduct a bit of research into how to 
+ * use smart pointers with c-style functions like memmap. Maybe a wrapper is needed, but how can it
+ * be implemented the most efficiently? Can the buffer idea from 1. be implemented in the wrapper
+ * for memmap? Is a new class as a wrapper even a good idea? (The characters are read one by one.
+ * If a function has to be called and an argument passed for every character, that would be an
+ * incredibly large amount of function calls and memory copied))
  *
  */
 
