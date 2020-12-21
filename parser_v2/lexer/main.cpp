0790bb8fdbbbc13ddda243b8b01b6d821ee2c859
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2020-12-21 18:26:29 +0000

    cleared unused code

--- parser_v2/lexer/main.cpp
+++ parser_v2/lexer/main.cpp
@@ -56,42 +56,6 @@ int main() {
 
 	Lexer l(s);
 
-/*	std::cout << "a: " << static_cast<int>('a') << std::endl;
-	std::cout << "b: " << static_cast<int>('z') << std::endl;
-	std::cout << "A: " << static_cast<int>('A') << std::endl;
-	std::cout << "Z: " << static_cast<int>('Z') << std::endl;
-	std::cout << "0: " << static_cast<int>('0') << std::endl;
-	std::cout << "9: " << static_cast<int>('9') << std::endl;
-	std::cout << "_: " << static_cast<int>('_') << std::endl;
-*/
-
-
-	/*std::cout << "[DEBUG] TokenType::openB: " << static_cast<int>(TokenType::openB) << std::endl;
-	std::cout << "[DEBUG] TokenType::closeB: " << static_cast<int>(TokenType::closeB) << std::endl;
-	std::cout << "[DEBUG] TokenType::eq: " << static_cast<int>(TokenType::eq) << std::endl;
-	std::cout << "[DEBUG] TokenType::slash: " << static_cast<int>(TokenType::slash) << std::endl;
-	std::cout << "[DEBUG] TokenType::comStart: " << static_cast<int>(TokenType::comStart) << std::endl;
-	std::cout << "[DEBUG] TokenType::comEnd: " << static_cast<int>(TokenType::comEnd) << std::endl;
-	std::cout << "[DEBUG] TokenType::value: " << static_cast<int>(TokenType::value) << std::endl;
-	std::cout << "[DEBUG] TokenType::ident: " << static_cast<int>(TokenType::ident) << std::endl;
-	std::cout << static_cast<int>(l.nextToken().type) << std::endl;
-	std::cout << static_cast<int>(l.nextToken().type) << std::endl;
-	std::cout << static_cast<int>(l.nextToken().type) << std::endl;
-	std::cout << static_cast<int>(l.nextToken().type) << std::endl;
-	std::cout << static_cast<int>(l.nextToken().type) << std::endl;
-	std::cout << static_cast<int>(l.nextToken().type) << std::endl;
-	std::cout << static_cast<int>(l.nextToken().type) << std::endl;
-
-	Token t = l.nextToken();
-
-	std::cout << static_cast<int>(t.type) << std::endl;
-	std::cout << t.content << std::endl;
-	
-	t = l.nextToken();
-
-	std::cout << static_cast<int>(t.type) << std::endl;
-	std::cout << t.content << std::endl;
-	*/
 
 	Token t;
 
