72b761297fd43bc15542f2bfa591a9a3ae9ce422
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2020-12-23 19:27:26 +0000

    Got the updated version of CSL_Interpreter.h and CSL_Interpreter.cp

--- include/Carbon/CSL_Interpreter.h
+++ include/Carbon/CSL_Interpreter.h
@@ -11,43 +11,50 @@ void OE_ReverseBitset(std::bitset<64>&);
 
 class CSL_Interpreter {
 public:
-	OE_World *interpret(std::string sourceCode);
-	OE_World *interpretFile(std::string pathToFile);
+	std::shared_ptr<OE_World> interpret(std::string sourceCode);
+	std::shared_ptr<OE_World> interpretFile(std::string pathToFile);
     
     ~CSL_Interpreter();
     
+    OE_SharedIndexMap<OE_Scene>          scenesList;
+    OE_SharedIndexMap<OE_Object>         objectsList;
+    OE_SharedIndexMap<OE_Material>       materialsList;
+    OE_SharedIndexMap<OE_Texture>        texturesList;
+    OE_SharedIndexMap<OE_TCM>            tcmsList;
+    OE_SharedIndexMap<OE_ViewportConfig> viewportsList;
+    
 private:
 
-//	//parser to parse the code before interpreting
-//	CSL_Parser *parser{nullptr};
-//
-//	//return value of "interpret()"
-//	//OE_World *world; UNNECESSARY
-//
-//	//a pointer to the CNode currently beeing processed
-//	CSL_Node *curNode{nullptr};
-//
-//	//a function for each type
-//	//the functions are called recursively (processWorld()->processScene()->processCamera->...)
-//	OE_World*          processWorld();
-//	OE_Scene*          processScene();
-//    OE_Camera*          processCamera();
-//    OE_Light*           processLight();
-//    //void processInteraction(OE_Interaction *interaction);
-//    OE_Mesh32 *         processMesh();
-//    //void processVertex(OE_Vertex *vertex);
-//    void processUVMapData(OE_UVMapData &uvmap, const std::size_t&);
-//    OE_VertexGroup *    processVertexGroup();
-//    //void processAnimation(OE_Animation *animation);
-//    //void processTransformation(OE_Transformation *transformation);
-//    void processTriangle(OE_Mesh32 *mesh, const std::size_t& num_of_uvs);
-//    //void processPhysics(OE_Physics *physics);
-//    OE_Texture * processTexture();
-//    OE_Material * processMaterial();
-//    OE_TCM * processTCM();
-//    void processTCM_Texture(OE_TCM_Texture &tcm_texture);
-//    OE_ViewportConfig * processViewportConfig();
+	//parser to parse the code before interpreting
+	CSL_Parser *parser{nullptr};
 
+	//return value of "interpret()"
+	//OE_World *world; UNNECESSARY
+
+	//a pointer to the CNode currently beeing processed
+	CSL_Node *curNode{nullptr};
+
+	//a function for each type
+	//the functions are called recursively (processWorld()->processScene()->processCamera->...)
+	std::shared_ptr<OE_World>          processWorld();
+	std::shared_ptr<OE_Scene>          processScene();
+    std::shared_ptr<OE_Camera>         processCamera();
+    std::shared_ptr<OE_Light>          processLight();
+    //void processInteraction(OE_Interaction *interaction);
+    std::shared_ptr<OE_Mesh32>         processMesh();
+    //void processVertex(OE_Vertex *vertex);
+    void processUVMapData(OE_UVMapData &uvmap, const std::size_t&);
+    OE_VertexGroup *    processVertexGroup();
+    //void processAnimation(OE_Animation *animation);
+    //void processTransformation(OE_Transformation *transformation);
+    void processTriangle(OE_Mesh32 *mesh, const std::size_t& num_of_uvs);
+    //void processPhysics(OE_Physics *physics);
+    std::shared_ptr<OE_Texture>  processTexture();
+    std::shared_ptr<OE_Material> processMaterial();
+    std::shared_ptr<OE_TCM>      processTCM();
+    void processTCM_Texture(OE_TCM_Texture &tcm_texture);
+    std::shared_ptr<OE_ViewportConfig> processViewportConfig();
+    
 };
 
 #endif
